07e64b91a02f8d3acb396f198f17a69bbb9f190d
syth0le <chdan565@gmail.com>
2021-11-08 22:34:24 +0300

    add basic usage of websockets

---
+++ basic_usage/server.py
@@ -0,0 +1,18 @@
+import asyncio
+import websockets
+
+
+async def say_hello(websocket, path):
+    name = await websocket.recv()
+    print(f'< {name}')
+
+    greeting = f'Hello {name}!'
+    await websocket.send(greeting)
+
+    print(f'> {greeting}')
+
+
+start_server = websockets.serve(say_hello, 'localhost', 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
